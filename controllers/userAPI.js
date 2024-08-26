c9ef828c2968565a6202757d4411adc631f0a7c9
William Tanaka <williamt@alunos.utfpr.edu.br>
2024-08-26 00:15:51 -0300

    Em desenvolvimento: ajustes nas regras de edição e exclusão de usuários
    Em desenvolvimento: ajustes nas regras de edição e exclusão de usuários

--- controllers/userAPI.js
+++ controllers/userAPI.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const router = express.Router();
-
 const { sucess, fail } = require("../helpers/resposta");
 const userDAO = require('../services/userDAO');
 const auth = require('../middlewares/auth');
 
-// Listar todos os usuários
+// Listar todos os usuários ou apenas usuários não administradores
 router.get("/", async (req, res) => {
-    let users = await userDAO.list();
+    let users;
+    users = await userDAO.list();
     res.json(sucess(users, "list"));
 });
 
 // Buscar usuário por ID
-router.get("/:id", async (req, res) => {
+router.get("/:id", auth, async (req, res) => {
     let user = await userDAO.getById(req.params.id);
     if (user)
         res.json(sucess(user));
@@ -24,6 +24,8 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
     const { username, password, email } = req.body;
 
+    // TODO: Validar os campos
+
     let user = await userDAO.save(username, password, email);
     if (user)
         res.json(sucess(user));
@@ -34,13 +36,19 @@ router.post("/", async (req, res) => {
 // Atualizar dados do usuário
 router.put("/:id", auth, async (req, res) => {
     const { id } = req.params;
-    const { username, email } = req.body;
-
-    let [result] = await userDAO.update(id, username, email);
-    if (result)
-        res.json(sucess(result));
-    else
-        res.status(500).json(fail("Falha ao alterar o usuário"));
+    const { username, email, password } = req.body;
+    
+    // Verifica se o usuário está tentando atualizar a si mesmo ou se é um administrador
+    if (req.user.isAdmin || req.user.id === id) {
+        // Atualiza o usuário
+        let updatedUser = await userDAO.update(id, username, email, password);
+        if (updatedUser)
+            res.json(sucess(updatedUser));
+        else
+            res.status(500).json(fail("Falha ao alterar o usuário"));
+    } else {
+        res.status(403).json(fail("Acesso negado"));
+    }
 });
 
 // Excluir usuário (apenas administradores podem)
--- services/userDAO.js
+++ services/userDAO.js
@@ -2,38 +2,32 @@ const UserModel = require('../models/user');
 
 module.exports = {
     list: async function() {
-        // Encontrar todos os usuários
-        return await UserModel.find();
+        return await UserModel.find({});
     },
-    
+
     save: async function(username, password, email, isAdmin = false) {
-        // Criar um novo usuário
-        const user = new UserModel({
-            username: username,
-            password: password,
-            email: email,
-            isAdmin: isAdmin
-        });
+        const user = new UserModel({ username, password, email, isAdmin });
         return await user.save();
     },
 
-    update: async function(id, username, email) {
-        // Atualizar um usuário existente
-        return await UserModel.findByIdAndUpdate(id, { username, email }, { new: true });
+    update: async function(id, username, email, password) {
+        const updateData = {};
+        if (username) updateData.username = username;
+        if (email) updateData.email = email;
+        if (password) updateData.password = password;
+
+        return await UserModel.findByIdAndUpdate(id, updateData, { new: true });
     },
 
     delete: async function(id) {
-        // Excluir um usuário
         return await UserModel.findByIdAndDelete(id);
     },
 
     getById: async function(id) {
-        // Encontrar um usuário por ID
         return await UserModel.findById(id);
     },
 
     getByUsername: async function(username) {
-        // Encontrar um usuário por username
         return await UserModel.findOne({ username });
     }
 };
