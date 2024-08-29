d2588b94e7b9e9f50aa435663c5008673e74cd07
William Tanaka <williamt@alunos.utfpr.edu.br>
2024-08-29 00:59:38 -0300

    Ajuste para execução da conexão com banco localmente
    Ajuste para execução da conexão com banco localmente

--- helpers/database.js
+++ helpers/database.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose")
 
 module.exports = (req, res, next) => {
-    mongoose.connect("mongodb+srv://admin:admin123@projetobe.pimfa.mongodb.net/?retryWrites=true&w=majority&appName=projetobe").catch((err) => {
+    // Conexão via cloud
+    //mongoose.connect("mongodb+srv://admin:admin123@projetobe.pimfa.mongodb.net/?retryWrites=true&w=majority&appName=projetobe").catch((err) => {
+    
+    // Conexão local
+    mongoose.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.0").catch((err) => {
         console.log("Error ao conectar no banco...")
     })
     return next()    
